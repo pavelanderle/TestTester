d6eebfa60aeaa497df77dc45ec3ef7bbf3e07561
Pavel <anderle@spseplyen.cz>
2020-12-08 12:06:59 +0100

    update rectangle test

--- tests/rectangleTest.php
+++ tests/rectangleTest.php
@@ -6,6 +6,11 @@ require '../src/rectangle.php';
 
 // obecný oblong
 $rect = new Rectangle(10, 20);
+$rect1 = new Rectangle(10, 20);
 Assert::same(200, $rect->getArea());  # ověříme očekávané výsledky
+Assert::equal($rect, $rect1);
 Assert::false($rect->isSquare());
+Assert::truthy($rect->getArea());
+Assert::nan($rect->getArea());
+Assert::match('Test%a?%', 'Test01');
 ?>
